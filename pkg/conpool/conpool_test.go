a6ff7130de1214d69cb56c7981645eb5aaa2ebc1
patrick.yu <patrick.yu@kakaoenterprise.com>
2022-07-13 10:37:24 +0900

    update conpool test

--- pkg/conpool/conpool_test.go
+++ pkg/conpool/conpool_test.go
@@ -1,8 +1,8 @@
 package conpool
 
 import (
+	"sync"
 	"testing"
-	"time"
 )
 
 // func TestConPool(t *testing.T) {
@@ -20,10 +20,12 @@ import (
 
 func senderMock(p ...interface{}) {
 	c, _ := p[0].(chan int)
+	wg, _ := p[1].(*sync.WaitGroup)
 	for i := 0; i < 500; i++ {
 		c <- i
 		<-c
 	}
+	wg.Done()
 }
 
 func BenchmarkConPool(b *testing.B) {
@@ -31,22 +33,29 @@ func BenchmarkConPool(b *testing.B) {
 		IdleWorkers: 10,
 		MaxWorkers:  10,
 	})
+	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
 		for j := 0; j < 10; j++ {
+			wg.Add(1)
 			c := make(chan int, 1)
-			pool.Submit(senderMock, c)
+			pool.Submit(senderMock, c, &wg)
 		}
-		time.Sleep(time.Millisecond * 20)
+		wg.Wait()
+		// time.Sleep(time.Millisecond * 20)
 	}
 }
 
 func BenchmarkGoroutine(b *testing.B) {
 	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
 		for j := 0; j < 10; j++ {
+			wg.Add(1)
 			c := make(chan int, 1)
-			go senderMock(c)
+			go senderMock(c, &wg)
 		}
-		time.Sleep(time.Millisecond * 20)
+		wg.Wait()
+		// time.Sleep(time.Millisecond * 20)
 	}
 }
